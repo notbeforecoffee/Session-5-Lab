af3a3d6e421c43418d0b71774df4bff9a9cdcc94
notbeforecoffee <theqcontinuum@msn.com>
2023-06-01 18:14:52 -0400

    updates modules

--- modules/circle.js
+++ modules/circle.js
@@ -1,7 +1,7 @@
 // This file has several modules, or a pieces of code we intend to use elsewhere. Any code that we can reuse in multiple places in our app, we want to store it in a communal file, or module, to import elsewhere in our application
 
 
-// here we are 'destructuring' Math, to get only the function PI from it.  Math is a built in  object in Javascript, and has several properties we can access and use
+// here we are 'destructuring' Math, to get only the property PI  from it.  Think of PI like a pre-supplied function, or method.  Math is a built in  object in Javascript, and has several properties we can access and use
 
 //Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
